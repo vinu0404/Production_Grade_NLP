d67baa220f69053c746a55f35754ab94a5c1fb3c
vinu0404 <22cd3034@rgipt.ac.in>
2025-01-09 18:40:52 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="vinu",
+    packages=find_packages(),
+    install_requires=[],
+)
